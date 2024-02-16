10cca3ad21b8bb43779fd98ee7202479997e063e
William Zehren <williamzehrentravail@gmail.com>
2024-02-16 11:32:46 +0100

    ajoutPug

--- tp1/app.js
+++ tp1/app.js
@@ -9,6 +9,9 @@ const port = 8000;
 const defaut = require('./routes/default');
 const form = require('./routes/form');
 
+app.set("view engine", "pug");
+app.set("views",path.join(__dirname,"views"));
+
 
 app.listen(port);
 app.use(bodyParser.urlencoded({extended : false}));
@@ -23,7 +26,8 @@ console.log("Serveur démarré sur le port : " + port);
 //toujours mettre les erreurs a la fin
 app.use((req,res,next)=>{
     console.log("404");
-    res.status(404).sendFile(path.join(__dirname,'views','page404.html'));
+    //res.status(404).sendFile(path.join(__dirname,'views','page404.html'));
+    res.status(404).render("page404");
 });
 
 
